e4d80885177e350079d53fd7fce1fc7675c40152
HeadAech <hubert.klonowski@gmail.com>
2021-02-09 14:35:21 +0100

    poprawka

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,8 +10,8 @@ public class Main {
 
         Random random = new Random();
         for(int i = 0; i < 15; i++){
-            float length = random.nextInt(200);
-            float width = random.nextInt(200);
+            float length = random.nextInt(100);
+            float width = random.nextInt(100);
 
             rectangles.add(new Rectangle(length, width));
         }
@@ -20,14 +20,14 @@ public class Main {
         List<Rectangle> zad_a = rectangles.stream()
                 .filter(rect -> rect.getArea() > rect.getPerimeter())
                 .collect(Collectors.toList());
-        System.out.println("ZAD A)");
+        System.out.println("\nZAD A)");
         System.out.println(zad_a);
 
         //zad b
         List<Rectangle> zad_b = rectangles.stream()
                 .sorted(Comparator.comparingDouble(Rectangle::getPerimeter))
                 .collect(Collectors.toList());
-        System.out.println("ZAD B)");
+        System.out.println("\nZAD B)");
         System.out.println(zad_b);
 
         //zad c
@@ -36,16 +36,21 @@ public class Main {
                 .average()
                 .getAsDouble();
 
-        System.out.println("ZAD C)");
+        System.out.println("\nZAD C)");
         System.out.println(zad_c);
 
         //zad d
+        Double avg_area = rectangles.stream()
+                .mapToDouble(Rectangle::getArea)
+                .average()
+                .getAsDouble();
+
         List<Rectangle> zad_d = rectangles.stream()
-                .filter(rect -> rect.getArea() < zad_c)
+                .filter(rect -> rect.getArea() < avg_area)
                 .sorted(Comparator.comparing(Rectangle::getArea).reversed())
                 .collect(Collectors.toList());
 
-        System.out.println("ZAD D)");
+        System.out.println("\nZAD D)");
         System.out.println(zad_d);
 
         //zad e
@@ -53,11 +58,11 @@ public class Main {
                 .mapToDouble(Rectangle::getArea)
                 .sum();
 
-        System.out.println("ZAD E)");
+        System.out.println("\nZAD E)");
         System.out.println(zad_e);
 
         //MAPA
-        System.out.println("MAPA");
+        System.out.println("\nMAPA");
         Map<Rectangle,Double> mapa = rectangles.stream()
                 .collect(Collectors.toMap( rect -> rect, Rectangle::getArea));
 
